9b10ac521f4bf2abb40f98e07b7681cde250b905
unitvector2021 <vitoon.sdf@gmail.com>
2022-02-02 11:58:28 +0700

    เพิ่ม

--- WEEK4/Pro1_64010806_4.java
+++ WEEK4/Pro1_64010806_4.java
@@ -1,23 +1,15 @@
-import java.util.*;
 public class Pro1_64010806_4{
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Symbol: ");
-        String symbol = input.nextLine();
-        System.out.print("Name: ");
-        String name = input.nextLine();
-
+        String symbol = "ORCL";
+        String name = " Oracle Coperation";
         Stock stock = new Stock(symbol,name);
-        try{
-        System.out.print("Previous Closing Price: ");
-        stock.previousClosingPrice = input.nextDouble();
-        System.out.print("Current Price: ");
-        stock.currentPrice = input.nextDouble();
-        System.out.print("Price Change: " + stock.getChangePercent() + "%");
-        }catch(Exception e){
-            System.out.println("You have to input in decimal number");
-        }
-        input.close();
-
+        stock.previousClosingPrice = 34.5;
+        stock.currentPrice = 34.35;
+        System.out.println("symbol: " + stock.symbol);
+        System.out.println("name: " + stock.name);
+        System.out.println("Previous Closing Price: " + stock.previousClosingPrice);
+        System.out.println("Current Price: " + stock.currentPrice);
+        System.out.println("Price Change: " + stock.getChangePercent() + "%");
+        
     }
 }
