adb2ae4ff147e06527b11f47df144fc4608d0fd5
Withun Watcharakritwekhin <vitoon.sdf@gmail.com>
2022-03-01 15:00:00 +0700

    WEEK8

--- WEEK8/Pro4/Pro4_64010806_8.java
+++ WEEK8/Pro4/Pro4_64010806_8.java
@@ -4,9 +4,12 @@ public class Pro4_64010806_8 {
         Octagon octagon1 = new Octagon(3);
         System.out.println("Octagon Area: " + octagon1.getArea());
         System.out.println("Octagon Perimeter : " + octagon1.getPerimeter());
+        System.out.println();
 
         Octagon octagon2 = (Octagon)octagon1.clone();
-        
+        System.out.println("Octagon clone Area: " + octagon2.getArea());
+        System.out.println("Octagon clone Perimeter : " + octagon2.getPerimeter());
+        System.out.println();
         int result = octagon1.compareTo(octagon2);
         if(result == 1) System.out.println("Octagon is greather than its clone.");
         else if(result == -1) System.out.println("Octagon is less than its clone.");
